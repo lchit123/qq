f97d277eb3adca2be3f7e745d47f6ebbc95f975b
zhuyuan <1094663847@qq.com>
2020-03-26 14:57:29 +0800

    登陆

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+class UserController extends Controller
+
+
 class IndexController extends Controller
 {
     public function index()
@@ -12,15 +15,7 @@ class IndexController extends Controller
         return view("/index/index");
     }
 
-    public function wx(){
-
-        $uid=uniqid();
-        $url="http://zzy.chatroom.13366737021.top/imag?uid=".$uid;
-        $obj->png($url, './1.png');
-        $obj = new QRcode();
-    }
+ 
 
 
 }
-
-
