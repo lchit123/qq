a49ce7bbe2c5459148d42f280a1c9afa41cada67
zhuyuan <1094663847@qq.com>
2020-03-26 15:02:06 +0800

    3

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,21 +1,13 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
-
-
 class IndexController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         return view("/index/index");
     }
-
- 
-
 
 }
