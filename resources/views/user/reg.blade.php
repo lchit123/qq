de892f48b2f18bbbce85e7f1c839e4ec6f9b5643
zhuyuan <1094663847@qq.com>
2020-03-30 09:39:54 +0800

    10

--- resources/views/user/reg.blade.php
+++ resources/views/user/reg.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>注册</title>
+    <title>注册页面</title>
 </head>
 <body>
 <div style="width:400px;height:500px;border: blue solid 3px;margin:100px 40%;">
