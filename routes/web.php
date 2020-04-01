1ac562dd77da6364c6edd175caae1f5829e80832
zhuyuan <1094663847@qq.com>
2020-04-01 13:27:35 +0800

    33

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/detail/{id}',"IndexController@detail");//详情页
-Route::get('/novelType/{id}',"IndexController@novelType");//分类详情页
 
-
-Route::get('/reg',"UserController@reg");//注册页面
-Route::post('/reg',"UserController@regDo");//注册执行
-Route::get('/login',"UserController@login");//登录页面
+//首页
+Route::get('/',"IndexController@index");
+//微信
+Route::get('/login',"UserController@login");
+Route::post('/user/ loginss',"UserController@ loginss");
+Route::get('/aouth',"UserController@aouth");
+Route::get('/oauth',"UserController@oauth");
+Route::get('/login1',"UserController@login1");//登录页面
 Route::post('/login',"UserController@loginDo");//登录执行
+Route::post('/getcode',"UserController@getcode");
+Route::post('/phonenum',"UserController@phonenum");
 
-Route::post('/getcode',"UserController@getcode");//获取验证码
-Route::post('/phonenum',"UserController@phonenum");//手机号唯一
-
-
-
-
-Route::get('/user/aouth',"UserController@aouth");
-Route::get('/user/oauth',"UserController@oauth");//自动触发 扫码登录
-
-Route::get('/qrcode',"IndexController@qrcode");//生成二维码
-Route::get('/image',"IndexController@image");//生成二维码
+//注册
+Route::get('/user/reg',"UserController@reg");
+Route::post('/user/regDo',"UserController@regDo");
